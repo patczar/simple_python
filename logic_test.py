668df25d57159e5c77bbd4fd77bd3e89df325425
Patryk Czarnik <p.czarnik@alx.pl>
2025-12-15 23:03:28 +0100

    Poprawka testów

--- logic_test.py
+++ logic_test.py
@@ -1,12 +1,12 @@
 from logic import *
 
 def test_hello():
-    res = hello('Ala')
+    res = say_hello('Ala')
     assert res == 'Hello Ala'
 
 
 def test_silnia_0():
-    assert silnia(0) == 1
+    assert factorial(0) == 1
 
 def test_silnia_5():
-    assert silnia(5) == 120
+    assert factorial(5) == 120
